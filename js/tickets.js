1f93d4eb65ab84a282890fb3634fb7c3a5f1a88c
Camila Heredia <cs.herediavalverde@gmail.com>
2022-10-26 11:26:18 -0300

    :bug: Fix: Fix event associated to Resumen button

--- js/tickets.js
+++ js/tickets.js
@@ -2,32 +2,26 @@ let txtMonto = document.getElementById('txt_monto');
 let btnBorrar = document.getElementById('btn-borrar');
 let btnResumen = document.getElementById('btn-resumen');
 
-let cantidadTickets = document.getElementById('input-cantidad').value;
-let select = document.getElementById('categoria');
-let indiceCategoria = select.selectedIndex;
-let nombreCategoria = select.options[indiceCategoria].text;
-
 btnResumen.addEventListener(
     'click',
-    function(){
-        calcularMonto();
+    function calcularMonto(){
+        let cantidadTickets = document.getElementById('input-cantidad').value;
+        let select = document.getElementById('categoria');
+        let nombreCategoria = select.options[select.selectedIndex].text;
+        let totalPagar;
+    
+        switch(nombreCategoria){
+            case "Estudiante":
+                totalPagar = (200 - (200 * 0.8)) * cantidadTickets;
+                break;   
+            case "Trainee":
+                totalPagar = (200 - (200 * 0.5)) * cantidadTickets;
+                break;
+            default:
+                totalPagar = (200 - (200 * 0.15)) * cantidadTickets;
+                break;
+        }
+    
+        txtMonto.innerText = `$ ${totalPagar}`;
     }
 )
-
-function calcularMonto(){
-    let totalPagar;
-
-    switch(nombreCategoria){
-        case "Estudiante":
-            totalPagar = (200 - (200 * 0.8)) * cantidadTickets;
-            break;   
-        case "Trainee":
-            totalPagar = (200 - (200 * 0.5)) * cantidadTickets;
-            break;
-        default:
-            totalPagar = (200 - (200 * 0.15)) * cantidadTickets;
-            break;
-    }
-
-    txtMonto.innerText = `$ ${totalPagar}`;
-}
